68b1c6395668e8e63ca3244d5587f06926c71538
scstatgit <seungchan.statistics@gmail.com>
2024-07-23 21:37:14 +0900

    add example

--- R/nested_if_else.R
+++ R/nested_if_else.R
@@ -1,4 +1,19 @@
-# https://www.r-bloggers.com/a-wrapper-around-nested-ifelse/
+#' source: https://www.r-bloggers.com/a-wrapper-around-nested-ifelse/
+#' nested_if_else() Function
+#'
+#' This function find formula from string
+#' @param if_stat, then
+#' @keywords nested_if_else
+#' @export
+#' @examples
+#' ie(
+#'   i(DATA$EDUC %in% c(1,2), 1),
+#'   i(DATA$EDUC==3, 2),
+#'   i(DATA$EDUC==4, 3),
+#'   e(888)
+#' )
+#'
+#' @export
 i <- function(if_stat, then) {
   if_stat <- lazyeval::expr_text(if_stat)
   then    <- lazyeval::expr_text(then)
@@ -10,7 +25,7 @@ e <- function(else_ret) {
 }
 ie <- function(...) {
   args <- list(...)
-  
+
   for (i in 1:(length(args) - 1) ) {
     if (substr(args[[i]], 1, 6) != "ifelse") {
       stop("All but the last argument, need to be i functions.", call. = FALSE)
