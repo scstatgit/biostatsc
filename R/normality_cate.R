57fbcfde937c1984b5c4a80e8ff92d9c886a2904
scstatgit <seungchan.statistics@gmail.com>
2024-07-28 19:35:19 +0900

    for check normality

---
+++ R/normality_cate.R
@@ -0,0 +1,11 @@
+# Function to check normality for categorical variables
+normality.cate <- function(DATA, STRATA, VARIABLE) {
+  xt <- table(unlist(subset(DATA, select = STRATA)), unlist(subset(DATA, select = VARIABLE)))
+  xt.e5 <- sum(chisq.test(xt)$expected < 5)
+  xt.nn <- prod(dim(xt))
+  if (xt.e5 / xt.nn < 0.2) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
---
+++ R/normality_num.R
@@ -0,0 +1,12 @@
+# Function to check normality for numerical variables
+normality.num <- function(DATA, VARIABLE) {
+  nn <- length(VARIABLE)  # Added this line to define nn
+  if (nn < 5000) {
+    pval <- shapiro.test(DATA[[VARIABLE]])$p.value
+    res <- (pval <= 0.05)
+  } else {
+    pval <- ks.test(DATA[[VARIABLE]], pnorm)$p.value
+    res <- (pval <= 0.05)
+  }
+  return(res)
+}
